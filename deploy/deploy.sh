dca891259ca671d810eef20be76269c5b8a10a8b
Jonathon Lee <jonathon.lee@JEUKFPHMAC7583.local>
2015-12-13 19:07:54 +0000

    Fixed deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: ./deploy.sh <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <BUCKET_NAME>"
+    echo "Usage: ./deploy.sh <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <ENVIRONMENT>"
     exit 1
 fi
 
@@ -18,23 +18,21 @@ TARGET_ENVIRONMENT_ALIAS=$3
 
 cd ${0%/*}
 
-source ~/.bashrc
-
-/home/ubuntu/.rbenv/shims/bundle install
+bundle install
 #bundle install
 
 cat > parameters.json <<EOF
 [
   {
-    "parameter_key": "TargeEnvironmentAlias",
+    "parameter_key": "TargeEnvironment",
     "parameter_value": $TARGET_ENVIRONMENT_ALIAS
   }
 ]
 EOF
 
 
-/home/ubuntu/.rbenv/shims/stackup $STACK_NAME up  --parameters "parameters.json" --template cloudformation.template
+stackup $STACK_NAME up  --parameters "parameters.json" --template webapp.template
 #stackup $STACK_NAME up  --parameters "parameters.json" --template cloudformation.template
-BUCKET_NAME=$(/home/ubuntu/.rbenv/shims/stackup $STACK_NAME outputs | /usr/bin/jq -r '.BucketName')
+BUCKET_NAME=$(stackup $STACK_NAME outputs | jq -r '.BucketName')
 #BUCKET_NAME=$(stackup $STACK_NAME outputs | jq -r '.BucketName')
-aws s3 cp ../dist.prod/ s3://$BUCKET_NAME/ --recursive
+aws s3 cp ../dist/ s3://$BUCKET_NAME/ --recursive
