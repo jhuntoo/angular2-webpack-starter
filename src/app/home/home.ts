b5e73aa98e8ecbf30f32fca239c173de2d7db664
Jonathon Lee <jonnolee84@gmail.com>
2016-03-11 00:09:16 +0000

    added first cut of searchbar

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,6 +3,7 @@ import {FORM_DIRECTIVES} from 'angular2/common';
 
 import {Title} from './services/title';
 import {XLarge} from './directives/x-large';
+import {SearchBar} from '../searchbar/searchbar';
 
 @Component({
   // The selector is what angular internally uses
@@ -17,6 +18,7 @@ import {XLarge} from './directives/x-large';
   // Doing so will allow Angular to attach our behavior to an element
   directives: [
     ...FORM_DIRECTIVES,
+    SearchBar,
     XLarge
   ],
   // We need to tell Angular's compiler which custom pipes are in our template.
---
+++ src/app/searchbar/searchbar.ts
@@ -0,0 +1,36 @@
+import {Component} from 'angular2/core';
+import {LoggingService} from "../common/log";
+import {Http} from "angular2/http";
+import {Router} from "angular2/router";
+import {Logger} from "../common/log";
+
+
+@Component({
+
+  selector: 'searchbar',
+  styles: [require('./searchbar.less').toString()],
+  template: require('./searchbar.html')
+})
+export class SearchBar {
+  log:Logger;
+
+
+  constructor(private _router: Router,private http:Http, logginService: LoggingService) {
+    let log : Logger = logginService.getLogger('RegisterForm');
+
+    this.log = log;
+
+
+
+  }
+
+
+  ngOnInit() {
+    this.log.debug('hello `SearchBar` component');
+    // this.title.getData().subscribe(data => this.data = data);
+  }
+
+
+
+
+}
